a9e8865619382cd9aee548d4cbe65f4def53a2ea
anim710 <anim3654@gmail.com>
2026-06-01 22:31:21 +0600

    solved login issue

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from 'better-auth';
 
 export const auth = betterAuth({
   secret:  process.env.BETTER_AUTH_SECRET,
-  baseURL: process.env.BETTER_AUTH_URL,
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
 
   socialProviders: {
     google: {
